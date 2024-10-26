cb0959885fb74707cfb3a1b040a0532f49efacef
Mitri <hmirseid54@gmail.com>
2024-10-26 15:37:02 +0400

    deyisilikler1

--- while.js
+++ while.js
@@ -11,6 +11,6 @@ let x2 = Math.round(-b - sqrt) / 2 * a * c
 if ((b ** 2) < (4 * a * c)) {
     document.getElementById("x1").innerHTML = `(b**2 - 4*a*c) bu dustura gore b-nin kvadrati 4ac den boyuk olmalidir, yazdigiz ededlere gore b-nin kvadrati 4ac den balaca olur ona gore de x1 nen x2 tapilmir`
 } else {
-    document.getElementById("x1").innerHTML = `Eger a-${a}, b-${b}, c ise-${c} olarsa onda x1-${x1} olar`
-    document.getElementById("x2").innerHTML = `Eger a-${a}, b-${b}, c ise-${c} olarsa onda x1- (${x2}) olar`
+    document.getElementById("x1").innerHTML = `Eger a-${a}, b-${b}, c ise-${c} olarsa onda x1- (${x1}) olar`
+    document.getElementById("x2").innerHTML = `Eger a-${a}, b-${b}, c ise-${c} olarsa onda x2- (${x2}) olar`
 }
